7e808df598a0e77e9f407842b3ed5dc0fbcfb871
Axel <info@axescontracting.com>
2026-09-19 01:24:32 -0700

    feat: Urartuhi lives here - resident whisper

---
+++ public/js/urartuhi-live.js
@@ -0,0 +1,5 @@
+﻿<script>
+window.URARTUHI_BACKEND = "https://refreshing-strength-production.up.railway.app/api/narrate";
+window.URARTUHI_LIVES_HERE = true;
+</script>
+<script src="/public/js/urartuhi-docent.js" defer></script>
